14d6e476277da449500993a63e045b64b2801c2a
ABaulderstone <alex.baulderstone@gmail.com>
2025-03-06 14:07:43 +1030

    adds post creation

--- blog/src/main/java/com/example/cli/BlogCLI.java
+++ blog/src/main/java/com/example/cli/BlogCLI.java
@@ -10,6 +10,7 @@ import org.jline.reader.LineReader;
 import com.example.posts.BlogPost;
 import com.example.posts.BlogPostService;
 import com.example.posts.Category;
+import com.example.posts.dtos.CreatePostDto;
 import com.example.posts.repository.BlogPostRepository;
 import com.example.posts.repository.InMemoryRepository;
 
@@ -48,11 +49,30 @@ public class BlogCLI {
 
     public void createPost() {
         helper.clearScreen();
+        CreatePostDto data = new CreatePostDto();
         System.out.println("Select a category");
-        var selected = helper.selectItemFromList(Arrays.asList(Category.values()));
-        System.out.println(selected);
+        Category selectedCategory = helper.selectItemFromList(Arrays.asList(Category.values()));
+        data.setCategory(selectedCategory);
+        helper.clearScreen();
+        System.out.println("Blog category selected");
         helper.pause();
+        String title = helper.validatedInput("Enter blog title", s -> !s.isBlank(), "Title cannot be empty");
+        data.setTitle(title);
+        helper.clearScreen();
+        System.out.println("Blog title created");
+        helper.pause();
+        String text = helper.validatedInput("Enter blog text", s -> !s.isBlank(), "Blog text cannot be empty");
+        data.setText(text);
+        service.createBlogPost(data);
+        helper.clearScreen();
+        System.out.println("Blog Post created");
+        helper.pause();
+    }
 
+    public void listPosts() {
+        var selectedPost = helper.selectItemFromList(service.getAllPosts());
+        System.out.println(selectedPost);
+        helper.pause();
     }
 
 }
